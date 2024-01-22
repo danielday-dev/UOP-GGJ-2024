d4bd7a40d94914d6146f3235a05eb605588b7d1d
danielday-dev <141579840+danielday-dev@users.noreply.github.com>
2024-01-22 19:22:22 +0000

    added more to the game

--- Source/Player.gd
+++ Source/Player.gd
@@ -2,27 +2,24 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var momentumDampening :float = 40;
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED * delta * momentumDampening)
 
+	var uppydowner = Input.get_axis("move_up", "move_down")
+	if uppydowner:
+		velocity.y = uppydowner * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED * delta * momentumDampening)
+		
 	move_and_slide()
